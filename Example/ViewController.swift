97f8ed189ee05e3ec98ffc1b6ad04973a2c262d3
acekiller <fengxijun51020@hotmail.com>
2020-11-12 22:04:53 +0800

    init project

---
+++ Example/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Mars Scala on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
